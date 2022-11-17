e0d45295ca4260b423c7bc813dd006d96e4ed23e
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2022-11-17 12:25:51 +0000

    api changes

--- tryvital/api/api_v1/endpoints/api.py
+++ tryvital/api/api_v1/endpoints/api.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+from datetime import date, datetime
 
 from fastapi import APIRouter
 from pydantic import BaseModel
@@ -11,21 +11,23 @@ router = APIRouter()
 
 class Activity(BaseModel):
     id: uuid.UUID
+    user_id: str
+    date: date
     steps: int
     calories: int
 
 
 @router.get("/activity")
 async def get_activity(
-    start_date: datetime,
-    end_date: datetime,
+    start_date: date,
+    end_date: date,
     user_id: str,
 ) -> Activity:
     """Get activity data for a user."""
     return "Success"
 
 
-@router.post("/fitbit/connect")
+@router.post("/fitbit/connect/{user_id}")
 async def connect_fitbit(user_id: str) -> str:
     """This endpoint is called when a user connects their Fitbit account."""
     credentials = Credentials(access_token="foo", refresh_token="bar")
