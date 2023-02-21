866f1cc2367b12fcff752c389a04b6683d8b60dc
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2023-02-21 13:44:35 +0000

    test example 1

--- oura_test_data.py
+++ oura_test_data.py
@@ -5,15 +5,29 @@ from .oura_sleep import SleepDocument
 test1 = [
     SleepDocument(
         id="1234",
-        contributors={},
+        contributors={
+            "efficiency": 78.0,
+        },
         day=date(2023, 1, 10),
         score=75.6,
         timestamp=datetime(2023, 1, 9, 22, 0, 0),
-        duration=3600 * 8,
+        duration=3600 * 6,
     ),
     SleepDocument(
         id="1234",
-        contributors={},
+        contributors={
+            "efficiency": 91.0,
+        },
+        day=date(2023, 1, 10),
+        score=75.6,
+        timestamp=datetime(2023, 1, 10, 4, 30, 0),
+        duration=3600 * 3,
+    ),
+    SleepDocument(
+        id="1234",
+        contributors={
+            "efficiency": 82.0,
+        },
         day=date(2023, 1, 9),
         score=75.6,
         timestamp=datetime(2023, 1, 9, 20, 0, 0),
